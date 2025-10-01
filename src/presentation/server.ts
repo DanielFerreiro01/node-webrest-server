835fa290149a3377e2e1f7218e5b3d60037e16c7
Daniel Ferreiro <daniiferreiro26@gmail.com>
2025-10-01 00:20:48 -0300

    Actualización CRUD

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    router: Router;
     public_path?: string;
 }
 
@@ -12,23 +13,30 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly router: Router;
 
     constructor(options: Options) {
-        const { port , public_path = 'public' } = options;
+        const { port , router, public_path = 'public' } = options;
         this.port = port;
+        this.router = router;
         this.publicPath = public_path;
     }
 
     async start() {
 
         //* Middlewares
-
+        this.app.use( express.json() );
+        this.app.use( express.urlencoded({ extended: true }) );
+        
         //* Public folder
         this.app.use(express.static( this.publicPath));
 
-        // Usar esta sintaxis para versiones nuevas de path-to-regexp
+        // Rutas
+        this.app.use(this.router);
+
+        // Cualquier ruta no definida pasa por aca
         this.app.get(/.*/, (req, res) => {
-            const indexPath = path.join(__dirname, `../../../${this.publicPath}/index.html`);
+            const indexPath = path.resolve(this.publicPath, 'index.html');
             res.sendFile(indexPath);
         });
 
